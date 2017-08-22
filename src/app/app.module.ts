4565653e3b04a6fe50b6d7701dd9e0660e62c0ad
pug3eye <pug3eye@gmail.com>
2017-08-23 02:36:13 +0700

    Using Services 4 push Notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -26,7 +27,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
