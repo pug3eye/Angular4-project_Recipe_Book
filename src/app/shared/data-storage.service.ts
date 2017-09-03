8aa86fe0af78a8005da6eacf8a38077bf7a5db59
pug3eye <pug3eye@gmail.com>
2017-09-04 02:36:35 +0700

    Finish HTTP Route

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -9,15 +10,25 @@ export class DataStorageService {
   constructor(private http: Http, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('https://pug-recipebook.firebaseio.com/recipe.json', this.recipeService.getRecipes());
+    return this.http.put('https://pug-recipebook.firebaseio.com/recipes.json', this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://pug-recipebook.firebaseio.com/recipe.json')
-      .subscribe(
+    this.http.get('https://pug-recipebook.firebaseio.com/recipes.json')
+      .map(
         (response: Response) => {
-            const recipes: Recipe[] = response.json();
-            this.recipeService.setRecipes(recipes);
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
         }
       );
   }
