583ad0aca47340d7e0d5457072a73654c4064809
pug3eye <pug3eye@gmail.com>
2017-09-05 01:46:15 +0700

    req&send token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,17 +4,24 @@ import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from './../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+            private authService: AuthService) {
+  }
 
   storeRecipes() {
     return this.http.put('https://pug-recipebook.firebaseio.com/recipes.json', this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://pug-recipebook.firebaseio.com/recipes.json')
+    // let tk = '';
+    const token = this.authService.getToken();
+
+    this.http.get('https://pug-recipebook.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
