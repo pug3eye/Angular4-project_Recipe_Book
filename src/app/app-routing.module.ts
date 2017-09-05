42de5942f079f4bf5d48bccece25d24d7f4fa68f
pug3eye <pug3eye@gmail.com>
2017-09-06 00:47:53 +0700

    Auth Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'shopping-list' , component: ShoppingListComponent },
-    { path: 'signup' , component: SignupComponent },
-    { path: 'signin' , component: SigninComponent },
+    { path: 'shopping-list' , component: ShoppingListComponent }
 ];
 
 @NgModule({
